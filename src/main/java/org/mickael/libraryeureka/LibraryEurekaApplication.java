a6ed276f369eedc2dfcc4cd5c475000f80cba3ec
Kamika-ZE <krysnn@gmail.com>
2020-05-21 02:42:28 +0200

    initial commit

---
+++ src/main/java/org/mickael/libraryeureka/LibraryEurekaApplication.java
@@ -0,0 +1,13 @@
+package org.mickael.libraryeureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryEurekaApplication.class, args);
+	}
+
+}
